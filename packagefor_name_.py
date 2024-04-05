55ca42d0b027b0eb0c65c552337136e7a1721191
shrishailhugar <48050309+shrishailhugar@users.noreply.github.com>
2024-04-05 23:51:03 +0530

    Update packagefor_name_.py

--- packagefor_name_.py
+++ packagefor_name_.py
@@ -1,5 +1,8 @@
 def callfun():
-    print("Hi i'm inside a function")
-
-
-callfun()
+    print(__name__ ,'\t inside callfun')
+if __name__=='__main__':
+    callfun()
+#
+# def callfun():
+#    print(__name__,' inside callfun')
+# callfun()
