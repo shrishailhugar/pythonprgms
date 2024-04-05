b9c873b2b379d6c483e56a5cd71394a65c0bce4d
shrishailhugar <48050309+shrishailhugar@users.noreply.github.com>
2024-04-05 23:50:33 +0530

    Update __name__main.py

--- __name__main.py
+++ __name__main.py
@@ -12,5 +12,24 @@ from packagefor_name_ import callfun
 # the above line not mean the file name should be __main__ each file if we are running it's main fun only 
 # it will call it from here only it will accept call from packagefor_name.py file bcz if we were runnning this file only this is __main__
 
-if '__name__'=='__main__':
+
+print(__name__' in source running file')
+callfun()
+
+#if we write callfun() in packagefor_name.py like this
+def callfun():
+   print('__name__',' inside callfun')
+callfun()
+#for 1st scenario it will yield
+#__name__.funfile  inside callfun ----- this is during import it will run fun
+#__main__                     ---- first print statement in this file(line 16)
+#__name__.funfile  inside callfun ---- this is when we call cllfun()(line 17)
+
+#if we write callfun() in packagefor_name.py like this
+def callfun():
+   print('__name__',' inside callfun')
+if __name__=='__main__'    
     callfun()
+# for 2nd scenario the output yield was
+# __main__                       --here during import it will not call bcz __name__!='__main__' as it's ran from this file(__name__main) (line 16)
+# __name__.funfile 	 inside callfun   --this is when callfun called(line 17)
